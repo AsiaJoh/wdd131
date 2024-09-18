6c7830084a36bea9327dbb15445f242c03121bfd
AsiaJoh <joh22056@byui.edu>
2024-09-17 20:26:14 -0600

    undid changes

--- script.js
+++
@@ -1,6 +0,0 @@
-const paragraph = document.getElementById('words');
-const button = document.getElementById('button-id');
-
-button.addEventListener('click', function() { 
-    paragraph.textContent = 'New words have appeared!';
-});
