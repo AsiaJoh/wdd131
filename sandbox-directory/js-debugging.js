b168c9e25e9d769a97c8e1c7c8ee87302676c675
AsiaJoh <joh22056@byui.edu>
2024-10-10 12:05:04 -0600

    Finished Activity 1

--- sandbox-directory/js-debugging.js
+++ sandbox-directory/js-debugging.js
@@ -1,8 +1,14 @@
 const PI = 3.14;
-const radius = 3;
+let radius = 3;
 let area = 0;
 area = radius * radius * PI;
 
-console.log(PI);
-console.log(radius);
-console.log(area);
+console.log("Pi:", PI);
+console.log("Radius1:", radius)
+console.log("Area1:", area)
+
+radius = 4;
+area = radius * radius * PI;
+
+console.log("Radius2:", radius)
+console.log("Area2:", area)
