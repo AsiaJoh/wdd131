4a0d4c1a47cd317e94259cb7e52540b9560050d0
AsiaJoh <joh22056@byui.edu>
2024-10-11 22:22:25 -0600

    Idk if I can figure this out

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -17,20 +17,30 @@ function viewHandler(event) {
     // create a variable to hold the element that was clicked on from event.target
     let viewerButton = document.querySelector(".close-viewer")
 
-	// get the src attribute from that element and 'split' it on the "-"
+    // Adding click event to the images which will rener the viewer upon being clicked
+    const imgElement = document.querySelectorAll("img") 
+    imgElement.addEventListener("click", function() {
+        // get the src attribute from that element and 'split' it on the "-"
+        const imgSrc = imgElement.getAttribute("src");
+        const parts = imgSrc.split("-");
 
+        // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+        const fullImg = `${parts[0]}-full.jpg`;
 
-	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+        // insert the viewerTemplate into the top of the body element
+        // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
 
-	// insert the viewerTemplate into the top of the body element
-	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImg, alt)) // Add arguments for viewer template???
 
-	// add a listener to the close button (X) that calls a function called closeViewer when clicked
-    viewerButton.addEventListener("click", closeViewer)
+        // add a listener to the close button (X) that calls a function called closeViewer when clicked
+        viewerButton.addEventListener("click", closeViewer)
+    })
 }
 
 gallery.addEventListener("click", viewHandler)
 
+
 function closeViewer() {
-    
+    const viewer = document.querySelector(".viewer");
+    viewer.remove();
 }
