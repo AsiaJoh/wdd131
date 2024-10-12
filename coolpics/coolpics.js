00053b645c4fd3a807bdcd417a243426ee307447
AsiaJoh <joh22056@byui.edu>
2024-10-11 22:40:01 -0600

    Progress thanks to ChatGPT??

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -9,38 +9,38 @@ menuButton.addEventListener("click", function ToggleMenu(){
 function viewerTemplate(pic, alt) {
     return `<div class="viewer">
         <button class="close-viewer">X</button>
-        <img src="norris-sm.jpeg" alt="picture">
-    </div>`
+        <img src="${pic}" alt="${alt}">
+    </div>`;
 }
 
 function viewHandler(event) {
+    // Adding click event to the images which will render the viewer upon being clicked
+    const imgElement = event.target.closest("img");
+    if (!imgElement) return; // Exit if not an image (remove this later??)
+    
+    // get the src attribute from that element and 'split' it on the "-"
+    const imgSrc = imgElement.getAttribute("src");
+    const parts = imgSrc.split("-");
+
+    // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const fullImg = `${parts[0]}-full.jpeg`;
+
+    // insert the viewerTemplate into the top of the body element
+    // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImg, imgElement.alt)); // Add arguments for viewer template???
+
     // create a variable to hold the element that was clicked on from event.target
     let viewerButton = document.querySelector(".close-viewer")
 
-    // Adding click event to the images which will rener the viewer upon being clicked
-    const imgElement = document.querySelectorAll("img") 
-    imgElement.addEventListener("click", function() {
-        // get the src attribute from that element and 'split' it on the "-"
-        const imgSrc = imgElement.getAttribute("src");
-        const parts = imgSrc.split("-");
-
-        // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-        const fullImg = `${parts[0]}-full.jpg`;
-
-        // insert the viewerTemplate into the top of the body element
-        // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
-
-        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImg, alt)) // Add arguments for viewer template???
-
-        // add a listener to the close button (X) that calls a function called closeViewer when clicked
-        viewerButton.addEventListener("click", closeViewer)
-    })
+    // add a listener to the close button (X) that calls a function called closeViewer when clicked
+    viewerButton.addEventListener("click", closeViewer)
 }
+
 
 gallery.addEventListener("click", viewHandler)
 
 
 function closeViewer() {
     const viewer = document.querySelector(".viewer");
-    viewer.remove();
+    if (viewer) viewer.remove(); // Remove the viewer if it exists
 }
