7897d23e36430e8307e3d41ab79630927ac261c9
AsiaJoh <joh22056@byui.edu>
2024-10-25 21:21:54 -0600

    cri

--- sandbox-directory/week-6/arrays.js
+++ sandbox-directory/week-6/arrays.js
@@ -1,7 +1,10 @@
 //  arrays.js
 const steps = ["one", "two", "three"];
-const listTemplate(step) {
-  return //the html string made from step
+const listTemplate(step) { steps.forEach(function callback(step))
+  
+  return `"<li>${step}</li>"`//the html string made from step
 }
-const stepsHtml = // use map to convert the list from strings to HTML
-document.querySelector("#myList").innerHTML = // set the innerHTML
+const stepsHtml = steps.map(function callback ( step)) { // use map to convert the list from strings to HTML
+  return step;
+} 
+document.querySelector("#myList").innerHTML = stepsHTML // set the innerHTML
