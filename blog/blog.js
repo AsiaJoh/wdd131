a8aa0d442aee5b517ee9a87b3a41e2f61402b4b2
AsiaJoh <joh22056@byui.edu>
2024-11-12 18:43:03 -0700

    djkafljadklfja

--- blog/blog.js
+++ blog/blog.js
@@ -25,3 +25,37 @@ const articles = [
 		stars: '⭐⭐⭐⭐'
 	}
 ]
+
+articles.forEach((article) => {
+	// Call a bunch of functions needed to create the blog
+
+	// Function to transform/put the data into that special string
+
+	// Function to change that special string into legitimate code for the html
+})
+
+function toString(article) {
+	const id = article["id"];
+	const title = article["title"]
+	const date = article["date"];
+	const description = article["description"];
+	const imgSrc = article["imgSrc"];
+	const imgAlt = article["imgAlt"];
+	const ages = article["ages"];
+	const genre = article["genre"];
+	const stars = article["stars"];
+
+	return `<article id="article1">
+            <section class="book-data">
+                <h4 class="date-published">July 5 2022</h4>
+                <p class="age-range">10-14</p>
+                <p class="genre">Fantasy</p>
+                <p class="rating">****</p>
+            </section>
+            <section class="book-showcase">
+                <h2>Septimus Heap Book One: Magyk</h2>
+                <img src="https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg" alt="Book cover for Septimus Heap 1">
+                <p class="summary">If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.</p>
+            </section>
+        </article>`
+}
